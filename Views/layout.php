137e21c0580615d1e64ce7e666713c079a41fd9c
Driss Zeroual <driss.it.zeroual@gmail.com>
2025-08-21 11:51:53 +0100

    add Layout : Layout template

---
+++ Views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Blog</title>
+</head>
+<body>
+    <h1>Mini Blog</h1>
+    <hr>
+    <?= $content ?>
+</body>
+</html>
